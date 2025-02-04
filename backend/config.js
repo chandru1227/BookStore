eaa319022b540f0f4965848980f5d917541ee723
Chandru Archunan <168585041+chandru1227@users.noreply.github.com>
2025-02-04 22:34:48 +0530

    Add files via upload

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://username:password@cluster0.ob2mnt5.mongodb.net/Book?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
